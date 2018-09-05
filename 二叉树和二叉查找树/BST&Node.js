62226903d4cf44cf7e7c3f1d41deeb7f0f900528
pan <1102847670@qq.com>
2018-09-05 11:34:10 +0800

    二叉树

--- 二叉树和二叉查找树/BST&Node.js
+++ 二叉树和二叉查找树/BST&Node.js
@@ -1,48 +1,156 @@
-//定义node节点类
-
-function Node(data,left,right){
+function Node(data, left, right) {
     this.data = data;
     this.left = left;
     this.right = right;
     this.show = show;
-}
-
-//展示数据
-function show(){
+ }
+ 
+ function show() {
     return this.data;
-}
-
-//定义BST(二叉树类)
-function BST(){
+ }
+ 
+ function BST() {
     this.root = null;
-    this.insert = insert;//插入节点方法
-    this.inorder = order;//中序遍历
-}
-
-//定义二叉查找树中插入数据方法
-function insert(data){
-    var n = new Node(data,null,null);
-    if(this.root == null){
-        this.root = n;
-    }else{
-        var current = this.root;
-        var parent;
-        while(true){
-            parent = current;
-            if(data < current.data){
-                current = current.left;
-                if(current == null){
-                    parent.left = n;
-                    break;
-                }
-            }else{
-                current = current.right;
-                if(current == null){
-                    parent.right = n;
-                    break;
-                }
-            }
-        }
+    this.insert = insert;
+    this.inOrder = inOrder;
+    this.preOrder = preOrder;
+    this.postOrder = postOrder;
+    this.getmin = getmin;
+    this.getmax = getmax;
+    this.find = find;
+    this.remove = remove;
+    this.removeNode = removeNode;
+    this.getSmallest = getSmallest;
+ }
+ 
+ function insert(data) {
+    var n = new Node(data, null, null);
+    if (this.root == null) {
+       this.root = n;
     }
-}
+    else {
+       var current = this.root;
+       var parent;
+       while (true) {
+          parent = current;
+          if (data < current.data) {
+             current = current.left;
+             if (current == null) {
+                parent.left = n;
+                break;
+             }
+          }
+          else {
+             current = current.right;
+             if (current == null) {
+                parent.right = n;
+                break;
+             }
+          }
+       }
+    }
+ }
+ 
+ function inOrder(node) {
+    if (!(node == null)) {
+       inOrder(node.left);
+       putstr(node.show() + " ");
+       inOrder(node.right);
+    }
+ }
+ 
+ function preOrder(node) {
+    if (!(node == null)) {
+       putstr(node.show() + " ");
+       preOrder(node.left);
+       preOrder(node.right);
+    }
+ }
+ 
+ function postOrder(node) {
+    if (!(node == null)) {
+       postOrder(node.left);
+       postOrder(node.right);
+       putstr(node.show() + " ");
+    }
+ }
+ 
+ function getmin() {
+    var current = this.root;
+    print("debug: " + current.data);
+    while (!(current.left == null)) {
+       current = current.left;
+    }
+    return current.data;
+ }
+ 
+ function getmax() {
+    var current = this.root;
+    while (!(current.right == null)) {
+       current = current.right;
+    }
+    return current.data;
+ }
+ 
+ function find(data) {
+    var current = this.root;
+    while (current.data != data) {
+       if (data < current.data) {
+          current = current.left;
+       }
+       else {
+          current = current.right;
+       }
+       if (current == null) {
+          return null;
+       }
+    }
+    return current;
+ }
+ 
+ function getSmallest(node) {
+    if (node.left == null) {
+       return node;
+    }
+    else {
+       return getSmallest(node.left);
+    }
+ }
+ 
+ function remove(data) {
+    root = removeNode(this.root, data);
+ }
+ 
+ function removeNode(node, data) {
+    if (node == null) {
+       return null;
+    }
+    if (data == node.data) {
+       // node has no children
+       if (node.left == null && node.right == null) {
+          return null;
+       }
+       // node has no left child
+       if (node.left == null) {
+          return node.right;
+       }
+       // node has no right child
+       if (node.right == null) {
+          return node.left;
+       }
+       // node has two children
+       var tempNode = getSmallest(node.right);
+       node.data = tempNode.data;
+       node.right = removeNode(node.right, tempNode.data);
+       return node;
+    }
+    else if (data < node.data) {
+       node.left = removeNode(node.left, data);
+       return node;
+    }
+    else {
+       node.right = removeNode(node.right, data);
+       return node;
+    }
+ }
 
